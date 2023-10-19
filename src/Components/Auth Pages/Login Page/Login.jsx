88ec7bc184d0a2927b12e570b21340561f9901a8
Adriyan-Ahmed-Jubayer <adriyan.khan.jubayer@gmail.com>
2023-10-19 16:20:03 +0600

    Login Functionality Added and Location Tracked for navigate

--- src/Components/Auth Pages/Login Page/Login.jsx
+++ src/Components/Auth Pages/Login Page/Login.jsx
@@ -1,14 +1,63 @@
 import { useContext } from "react";
 import { AuthContext } from "../../../Providers/Auth/AuthProvider";
 import { BsGoogle } from "react-icons/bs"
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate  } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const Login = () => {
+    const Location = useLocation();
+    const track = useNavigate();
+    const navigate = () => {
+        console.log(Location.state);
+         track(Location?.state ? Location.state : "/" )
+    }
+    const {LoginAccount, GoogleLogin} = useContext(AuthContext);
+    const handleLogin = e => {
+        e.preventDefault();
+        const form = e.target;
+        const email = form.email.value;
+        const pass = form.password.value;
+        LoginAccount(email, pass)
+        .then(res => {
+            console.log(res.user);
+            if(res){
+                toast.success('Login successful! You now have access. 🎉😊', {
+                    position: "top-center"
+                })
+                navigate()
+            }
+        })
+        .catch(err => {
+            if (err.message == 'Firebase: Error (auth/network-request-failed).') {
+                toast.error('Your Network Connection is Too Slow!')
+            }
+            else{
+                toast.error(err.message, {
+                    position: "top-center"
+                })
+                
+            }
+        })
+    }
+
+    const handleGGLLoigin = () =>{
+        GoogleLogin()
+        .then(res =>{
+            if(res){
+                toast.success('Login successful! You now have access. 🎉😊', {
+                    position: "top-center"
+                })
+            }
+        })
+        .catch(err => {
+            toast.error(err.message)
+        })
+    }
     return (
         <>
             <section className=" flex justify-center items-center">
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 card-body">
-                    <form className="">
+                    <form onSubmit={handleLogin} className="">
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
@@ -34,7 +83,7 @@ const Login = () => {
                         <div className="bg-violet-500 h-2 w-full"></div>
                     </div>
                     <div className="text-center mt-8">
-                        <button className="text-purple py-2 px-4 md:py-4 md:px-8 rounded-sm font-bold border-purple-600 border-2 w-full flex items-center justify-center gap-2 hover:text-purple-600"><BsGoogle></BsGoogle>Login With Google</button>
+                        <button onClick={handleGGLLoigin} className="text-purple py-2 px-4 md:py-4 md:px-8 rounded-sm font-bold border-purple-600 border-2 w-full flex items-center justify-center gap-2 hover:text-purple-600"><BsGoogle></BsGoogle>Login With Google</button>
                     </div>
                 </div>
             </section>
--- src/Shared/NavigationBar/Navigation.jsx
+++ src/Shared/NavigationBar/Navigation.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { Link, NavLink } from "react-router-dom";
 import { AuthContext } from "../../Providers/Auth/AuthProvider";
+import { toast } from "react-toastify";
 
 const Navigation = () => {
     const { User, LogOutAccount } = useContext(AuthContext);
